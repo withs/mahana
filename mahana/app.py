16610d94c0b1b6d393f700c4486ad812d6e3fe5c
Shib <antoine@quetel.pro>
2021-03-14 21:29:00 +0100

    feat(web server): change way to access database from global aiohttp

--- mahana/app.py
+++ mahana/app.py
@@ -1,12 +1,16 @@
+import jinja2
+import aiohttp_jinja2
+
 from configs.config import AppConfig, DatabaseConfig
 from motor.motor_asyncio import AsyncIOMotorClient
 from aiohttp import web
 
-import jinja2
-import aiohttp_jinja2
-
 from web.views import (
     Home
+)
+
+from classes import (
+    Mongo
 )
 
 
@@ -27,8 +31,9 @@ class Mahana:
         self.app.add_routes(routes)
 
     def _setup_mongo(self):
-        mongo_db_client = AsyncIOMotorClient(DatabaseConfig.CONNECT_URI)[DatabaseConfig.BASE_DB]
-        self.app["mongo"] = mongo_db_client
+        mongo_db_connection = AsyncIOMotorClient(DatabaseConfig.CONNECT_URI)
+        mongo_obj = Mongo(mongo_client=mongo_db_connection)
+        self.app["mongo_db"] = mongo_obj
 
 
     def _setup(self):
@@ -36,7 +41,7 @@ class Mahana:
 
         self._setup_jinja()
         self._setup_routes()
-        #self._setup_mongo()
+        self._setup_mongo()
 
 
 
