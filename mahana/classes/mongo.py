7b87f74c67b6ca0eaa63b7fae28ce95b48828eef
Shib <antoine@quetel.pro>
2021-03-17 21:25:22 +0100

    feat(db|command): change return of query from bool to dataclass with the querry result data is sucess

--- mahana/classes/mongo.py
+++ mahana/classes/mongo.py
@@ -1,3 +1,5 @@
+from dataclasses import make_dataclass
+
 from configs.config import DatabaseConfig
 
 
@@ -5,6 +7,7 @@ class Mongo:
     def __init__(self, mongo_client):
         self.mongo_db_client = mongo_client
         self.db = self.mongo_db_client[DatabaseConfig.BASE_DB]
+        self.data_class_resp = make_dataclass('DbResult', ["result", "result_data"])
 
 
     async def add_new_worker(self, name: str, display_name: str, worker_id: str) -> None:
@@ -28,17 +31,20 @@ class Mongo:
         collections_list = await self.db.list_collection_names()
         for collection in collections_list:
             collection = self.db.get_collection(collection)
-            collection_querry = await collection.find_one({"auth_keys": {"$in": [key]}})
-            if collection_querry is not None:
-                return True
-            return False
+            collection_querry = await collection.find_one({"auth_keys": {"$in": [key]}},{"_id": False})
 
-    async def find_by_worker_name(self, worker_name: str) -> bool:
+            if collection_querry is not None:
+                return self.data_class_resp(True, collection_querry)
+            return self.data_class_resp(False, collection_querry)
+
+
+
+    async def find_by_worker_name(self, worker_name: str):
 
         collections_list = await self.db.list_collection_names()
         for collection in collections_list:
             collection = self.db.get_collection(collection)
-            collection_querry = await collection.find_one({"worker_name": str(worker_name)})
+            collection_querry = await collection.find_one({"worker_name": str(worker_name)},{"_id": False})
             if collection_querry is not None:
-                return True
-            return False
+                return self.data_class_resp(True, collection_querry)
+            return self.data_class_resp(False, collection_querry)
