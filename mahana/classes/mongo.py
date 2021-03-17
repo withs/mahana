0190ee2a4db91755a6447cf577ce3c3060b3da30
Shib <antoine@quetel.pro>
2021-03-17 21:50:38 +0100

    fix(db|command): change argument from each info of the worker to a pre made to insert a en worker in the db
    also fix an indention error in the_by_name function which cause instant return a the first loop

--- mahana/classes/mongo.py
+++ mahana/classes/mongo.py
@@ -10,20 +10,13 @@ class Mongo:
         self.data_class_resp = make_dataclass('DbResult', ["result", "result_data"])
 
 
-    async def add_new_worker(self, name: str, display_name: str, worker_id: str) -> None:
+    async def add_new_worker(self, worker_dict: dict) -> None:
 
-        worker_collection = self.db[f"worker-{worker_id}"]
+        worker_collection = self.db[f"""worker-{worker_dict["worker_id"]}"""]
 
-        document = {
-            "woker_name": name,
-            "worker_display_name": display_name,
-            "worker_id": worker_id,
-            "last_data_sent": None,
-            "auth_keys": [],
-            "sensors": [],
 
-        }
-        await worker_collection.insert_one(document)
+        a = await worker_collection.insert_one(worker_dict)
+        print(a)
 
     async def find_auth_by_key(self, key: str) -> bool:
         # {"auth_keys": {$in: ["abc"]}}
@@ -35,16 +28,17 @@ class Mongo:
 
             if collection_querry is not None:
                 return self.data_class_resp(True, collection_querry)
-            return self.data_class_resp(False, collection_querry)
+        return self.data_class_resp(False, collection_querry)
 
 
 
     async def find_by_worker_name(self, worker_name: str):
 
+
         collections_list = await self.db.list_collection_names()
         for collection in collections_list:
             collection = self.db.get_collection(collection)
-            collection_querry = await collection.find_one({"worker_name": str(worker_name)},{"_id": False})
+            collection_querry = await collection.find_one({"worker_name": str(worker_name)})
             if collection_querry is not None:
                 return self.data_class_resp(True, collection_querry)
-            return self.data_class_resp(False, collection_querry)
+        return self.data_class_resp(False, collection_querry)
