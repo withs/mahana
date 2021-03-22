9ae4474241c7f3f704cb7b9b65efbb29e1b3e211
Shib <antoine@quetel.pro>
2021-03-22 19:22:54 +0100

    refactor(web|server): change view class name

--- mahana/classes/__init__.py
+++ mahana/classes/__init__.py
@@ -1,5 +1,7 @@
 from .mongo import Mongo
 from .utils import Utils
 from .decorators import Decorators
+from .worker import Worker
+from .sensor import Sensor
 
-__ALL__ = ["Mongo", "Utils", "Decorators"]
+__ALL__ = ["Mongo", "Utils", "Decorators", "Worker", "Sensor"]
