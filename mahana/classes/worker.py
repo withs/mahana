c1504261ec28a03f69a5182e0e45fd3cc4131f97
Shib <antoine@quetel.pro>
2021-03-22 23:21:21 +0100

    feat(class|worker): read description
    add aliases to access some attr in the class from the db name
    
    add default value in __init__ to attr
    
    add a method to update the db from the worker object

--- mahana/classes/worker.py
+++ mahana/classes/worker.py
@@ -6,27 +6,48 @@ from .utils import Utils
 
 class Worker:
 
+    aliases = {
+        "worker_id": "id",
+        "worker_name": "name",
+        "worker_display_name": "display_name",
+        "worker_config": "config"
+    }
+
     def __init__(self, worker_data, db_con):
-        self.name: str
-        self.display_name: str
-        self.id: str
-        self.last_data_sent: str
-        self.auth_keys: List
-        self.sensors: List
-        self.config: dict
+        self._name: str= None
+        self.display_name: str= None
+        self._id: str= None
+        self.last_data_sent: str= None
+        self.auth_keys: List= None
+        self.sensors: List= None
+        self.sensors_id: List= None
+        self.config: dict= None
 
         self.db = db_con
 
         self._from_data(worker_data)
 
     def _from_data(self, worker_data):
-        self.name = worker_data.get("worker_name", None)
+        self._name = worker_data.get("worker_name", None)
         self.display_name = worker_data.get("worker_display_name", None)
-        self.id = worker_data.get("worker_id", None)
+        self._id = worker_data.get("worker_id", None)
         self.last_data_sent = worker_data.get("last_data_sent", None)
-        self.sensors = None
         self.auth_keys = worker_data.get("auth_keys", None)
+        self.sensors_id = worker_data.get("sensors", None)
         self.config = worker_data.get("worker_config", None)
+
+    def to_dict(self):
+        worker_dict = {
+            "worker_name": self.name,
+            "worker_display_name": self.display_name,
+            "worker_id": self._id,
+            "last_data_sent": self.last_data_sent,
+            "sensors": self.sensors_id,
+            "auth_keys": self.auth_keys,
+            "worker_config": self.config
+        }
+
+        return worker_dict
 
     @classmethod
     async def create_new_worker(cls, new_worker_data, db_con):
@@ -54,21 +75,66 @@ class Worker:
             "last_data_sent": None,
             "auth_keys": [Utils.ur_safe_key(key_len=36)],
             "sensors": [],
+            "sensors_id": new_worker_data.get("sensors"),
             "worker_config": {
               "send_data_interval": 0 # prendre cette valuer depuis la config modulaire (config.toml)
             }
         }
-        create_new_req = await db_con.add_new_worker(new_worker)
+
+        new_worker_obj = cls(new_worker, db_con)
+        create_new_req = await db_con.add_new_worker(new_worker_obj)
         if create_new_req.result:
-            new_worker_obj = cls(new_worker, db_con)
             return (True, new_worker_obj)
 
         return (False, "db error")
 
+    async def update_db(self):
+        update_req = await self.db.edit_worker(self)
+
+        if update_req.result is True:
+            return (True, None)
+        return (False, None)
+
     def __repr__(self):
         return f"<Worker (name={self.name}, id={self.id}, display_name={self.display_name})>"
+
+    def __setitem__(self, key, value):
+        if key in self.__dict__ or key in Worker.aliases:
+            setattr(self, key, value)
+
+    def __getattribute__(self, name):
+        name = Worker.aliases.get(name, name)
+        return object.__getattribute__(self, name)
+
+    def __setattr__(self, name, value):
+        name = Worker.aliases.get(name, name)
+        object.__setattr__(self, name, value)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
 
     async def get_sensors(self):
         if self.sensors is None:
             await self.db.get_worker_sensors(self)
         return self.sensors
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, to_set):
+        if self._id is None:
+            self._id = to_set
+
+    @property
+    def name(self):
+        return self._name
+
+    async def change_name(self, new_name):
+        name_check = await self.db.find_worker(worker_name=new_name)
+        if name_check:
+            return self._name
+        if len(new_name) != 0:
+            self._name = new_name
